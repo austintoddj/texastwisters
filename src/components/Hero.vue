93602e1f76c2e8d8ad3a4f3c58ff10b07fb364fa
Todd Austin <austin.todd.j@gmail.com>
2023-07-24 10:26:30 -0500

    map updates

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -31,10 +31,18 @@
         <div class="py-24 sm:py-32">
             <div class="mx-auto max-w-7xl px-6 lg:px-8">
                 <div class="mx-auto max-w-2xl text-center">
+                    <h2
+                        class="text-base font-semibold leading-7 text-indigo-600"
+                    >
+                        Coming Soon
+                    </h2>
                     <h1
                         class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
                     >
-                        Texas Twisters Gymnastics
+                        <!-- <span class="italic text-gray-400">Coming Soon</span><br /> -->
+                        <span class="font-serif"
+                            >Texas Twisters Gymnastics, LLC</span
+                        >
                         <!-- Coming Soon -->
                     </h1>
                     <p class="mt-6 text-lg leading-8 text-gray-600">
@@ -43,7 +51,7 @@
                         stay in the loop? Sign up for our newsletter.
                     </p>
 
-                    <form
+                    <!-- <form
                         class="mt-10 max-w-md m-auto w-full col-span-5"
                         method="POST"
                         @submit.prevent="subscribe"
@@ -88,14 +96,14 @@
                                 >privacy&nbsp;policy</a
                             >.
                         </p>
-                    </form>
+                    </form> -->
                 </div>
                 <div class="mt-16 flow-root sm:mt-24">
                     <div
                         class="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4"
                     >
                         <img
-                            src="/building.jpg"
+                            src="/map.png"
                             alt="App screenshot"
                             width="2432"
                             height="1442"

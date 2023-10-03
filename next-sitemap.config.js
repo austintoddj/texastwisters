f9404a8c39986949f9e990edde9386490f3c1d9e
Todd Austin <austin.todd.j@gmail.com>
2023-10-03 16:23:49 -0500

    updating the sitemap site url to use env variables instead of deployment urls

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: new URL(`https://${process.env.VERCEL_URL}`),
+  siteUrl: new URL(`https://${process.env.NEXT_PUBLIC_DOMAIN_URL}`),
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   exclude: ['/opengraph-image.png']

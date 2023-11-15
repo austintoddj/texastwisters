05295696374e732ffec1e176b9ab85b55ffc0466
Todd Austin <austin.todd.j@gmail.com>
2023-11-15 10:27:11 -0600

    dependency updates

--- src/components/AboutHero.js
+++ src/components/AboutHero.js
@@ -66,23 +66,23 @@ export function AboutHero() {
             country, and have been influenced by many incredible coaches and
             athletes along the way.
           </p>
-            <p>
+          <p>
             While coaching at a gym that offered adult gymnastics, my husband
             Todd was introduced to the sport for the first time. Watching his
             talent and excitement grow inspired my passion to involve more
             adults in gymnastics.
           </p>
-            <p>
+          <p>
             Our daughter is now a competitive gymnast, and currently holds state
             titles on bars and vault. Our son loves baseball and is currently
             playing his fifth season with GYBA.
           </p>
-            <p>
+          <p>
             We moved from Minneapolis to Georgetown in 2018, and are proud to
             call it our home. We love Georgetown and can't wait to bring this
             business to you!
           </p>
-            <p>- Becky</p>
+          <p>- Becky</p>
         </div>
       </div>
     </section>

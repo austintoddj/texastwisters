37cb840bfba063d2371f41b262a1533c42427eab
Todd Austin <austin.todd.j@gmail.com>
2025-06-11 15:25:54 -0500

    policy page updates

--- src/app/policies/page.jsx
+++ src/app/policies/page.jsx
@@ -4,7 +4,7 @@ import { PolicyInformation } from '@/components/PolicyInformation'
 export const metadata = {
   title: 'Rules & Policies - Texas Twisters Gymnastics',
   description:
-    'We want to ensure that the gym has a positive and healthy culture, and this policy is designed with that in mind.',
+    'We strive to ensure that our gym maintains an equitable, positive, and healthy culture. These policies are designed with that in mind.',
   alternates: {
     canonical: './'
   }

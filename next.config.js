1fc07da8ff2cf53306e400b86cedae0e277e6f87
Todd Austin <austin.todd.j@gmail.com>
2025-10-27 14:14:04 -0500

    Enable verbose Sentry logs in Next.js config
    Set the 'silent' option to false in the Sentry configuration to allow verbose logging. This change is intended to help diagnose issues by providing more detailed logs.

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ module.exports = withSentryConfig(nextConfig, {
   // Sentry options from your original config
   org: process.env.SENTRY_ORG,
   project: process.env.SENTRY_PROJECT,
-  silent: !process.env.CI, // Only print logs in CI
+  silent: false, // Enable verbose logs to diagnose issues
   widenClientFileUpload: true, // Keep for prettier stack traces
   tunnelRoute: '/monitoring', // Keep to circumvent ad-blockers
   disableLogger: true, // Keep to reduce bundle size

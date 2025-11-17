b09f308fb86e08e669894980bd393553fc079ee2
Todd Austin <austin.todd.j@gmail.com>
2025-11-17 10:02:38 -0600

    Simplify siteUrl assignment in sitemap config
    Removed explicit domainUrl variable and error check. siteUrl now directly uses process.env.NEXT_PUBLIC_DOMAIN_URL for improved brevity.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
-const domainUrl = process.env.NEXT_PUBLIC_DOMAIN_URL
-
-if (!domainUrl) {
-  throw new Error(
-    'NEXT_PUBLIC_DOMAIN_URL environment variable is required for sitemap generation. Please check your .env.local file.'
-  )
-}
-
 module.exports = {
-  siteUrl: `https://${domainUrl}`,
+  siteUrl: `https://${process.env.NEXT_PUBLIC_DOMAIN_URL}`,
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   exclude: ['/opengraph-image.png']

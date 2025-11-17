fe0b24b4ad76d73e8d4c0c6e0311b1d625068d3c
Todd Austin <austin.todd.j@gmail.com>
2025-11-17 10:25:06 -0600

    Remove build workflow and update tests and sitemap config
    Deleted the build GitHub Actions workflow. Enhanced the tests workflow by adding caching for Next.js build artifacts. Simplified siteUrl logic in next-sitemap.config.js to use a string template instead of URL objects.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
-console.log('[v0] NEXT_PUBLIC_DOMAIN_URL:', process.env.NEXT_PUBLIC_DOMAIN_URL);
-
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_DOMAIN_URL 
-    ? new URL(`https://${process.env.NEXT_PUBLIC_DOMAIN_URL}`)
-    : new URL('https://localhost:3000'),
- generateRobotsTxt: true,
+  siteUrl: `https://${process.env.NEXT_PUBLIC_DOMAIN_URL}`,
+  generateRobotsTxt: true,
   generateIndexSitemap: false,
   exclude: ['/opengraph-image.png']
 }

1bc11bf51d84d715989d19d25d00bbf2c8d861bb
Todd Austin <austin.todd.j@gmail.com>
2025-11-17 10:06:09 -0600

    Improve siteUrl handling in sitemap config
    Added a console log for NEXT_PUBLIC_DOMAIN_URL and updated siteUrl assignment to use the URL constructor with a fallback to localhost if the environment variable is not set.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next-sitemap').IConfig} */
+console.log('[v0] NEXT_PUBLIC_DOMAIN_URL:', process.env.NEXT_PUBLIC_DOMAIN_URL);
+
 module.exports = {
-  siteUrl: `https://${process.env.NEXT_PUBLIC_DOMAIN_URL}`,
-  generateRobotsTxt: true,
+  siteUrl: process.env.NEXT_PUBLIC_DOMAIN_URL 
+    ? new URL(`https://${process.env.NEXT_PUBLIC_DOMAIN_URL}`)
+    : new URL('https://localhost:3000'),
+ generateRobotsTxt: true,
   generateIndexSitemap: false,
   exclude: ['/opengraph-image.png']
 }

fe9e5712eba111b5963467ac08630268741ebdd3
Todd Austin <austin.todd.j@gmail.com>
2026-04-23 17:14:25 -0500

    Append Content-Signal header to robots.txt
    Update next-sitemap.config.js to add a transformRobotsTxt function that appends a Content-Signal line (`ai-train=no, search=yes, ai-input=no`) to the generated robots.txt. Also adjusted the `exclude` entry formatting.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,8 @@ module.exports = {
   siteUrl: `https://${process.env.NEXT_PUBLIC_DOMAIN_URL}`,
   generateRobotsTxt: true,
   generateIndexSitemap: false,
-  exclude: ['/opengraph-image.png']
+  exclude: ['/opengraph-image.png'],
+  transformRobotsTxt: async (_, robotsTxt) => `${robotsTxt}
+Content-Signal: ai-train=no, search=yes, ai-input=no
+`
 }
